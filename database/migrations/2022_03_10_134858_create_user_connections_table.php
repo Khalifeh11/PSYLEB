e245b020e1bc209425dc659aabb9bae86f36f874
Karim <abdelkarim.kh95@gmail.com>
2022-03-10 15:52:27 +0200

    user connections migration

---
+++ database/migrations/2022_03_10_134858_create_user_connections_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserConnectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_connections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user1_id')
+            ->constrained("users")
+            ->onDelete("cascade");
+            $table->foreignId('user2_id')
+            ->constrained("users")
+            ->onDelete("cascade");
+            $table->boolean('is_pending')->default(0);
+            $table->boolean('is_blocked')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_connections');
+    }
+}
