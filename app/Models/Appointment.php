667bce70fa7661b77645cd983f8f8958b9742284
Karim <abdelkarim.kh95@gmail.com>
2022-03-10 15:48:33 +0200

    appointment model with inverse relationship to user class

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'date',
+        'provider_id',
+        'client_id',
+        'location_id'
+    ];
+
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
