fedd9eecd7b9b7bdba28ea3dde656822e7269221
Karim <abdelkarim.kh95@gmail.com>
2022-03-10 16:53:30 +0200

    user appointment model with relationship to user and location

--- app/Models/UserAppointment.php
+++ app/Models/UserAppointment.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Appointment extends Model
+class UserAppointment extends Model
 {
     use HasFactory;
 
@@ -17,9 +17,15 @@ class Appointment extends Model
         'location_id'
     ];
 
-    
-    public function user()
-    {
-        return $this->belongsTo(User::class);
+    public function providerAppointment(){
+        return $this->belongsTo(User::class, 'provider_id');
+    }
+
+    public function clientAppointment(){
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function appointmentLocation(){
+        return $this->belongsTo(UserLocation::class, 'location_id');
     }
 }
