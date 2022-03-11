bf500d25f8e39ed6208f2ab3a8619caa66f4bd83
Karim <abdelkarim.kh95@gmail.com>
2022-03-11 14:44:13 +0200

    user appointment factory

---
+++ database/factories/UserAppointmentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\UserLocation;
+
+
+class UserAppointmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $users = User::all()->pluck('id')->toArray();
+        $location = UserLocation::all()->pluck('id')->toArray();
+
+        return [
+            'name' => 'Appointment',
+            'date' => $this->faker->date(),
+            'provider_id' =>$this->faker->unique()->randomElement($users),
+            'client_id'=>$this->faker->unique()->randomElement($users),
+            'location_id'=>$this->faker->unique()->randomElement($location),
+        ];
+    }
+}
