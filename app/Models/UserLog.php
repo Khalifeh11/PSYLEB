6edb6a26e2c8f001d737a6925ae61b6f8c183350
Karim <abdelkarim.kh95@gmail.com>
2022-03-11 15:11:50 +0200

    user log protected fillables

--- app/Models/UserLog.php
+++ app/Models/UserLog.php
@@ -5,9 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class UserLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'mood',
+        'notes',
+        'sleep',
+        'user_id'
+    ];
 
     public function user()
     {
