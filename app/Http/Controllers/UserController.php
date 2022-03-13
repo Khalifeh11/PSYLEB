6f846165d74a56649e06581b61049cdfb211ed7e
Karim <abdelkarim.kh95@gmail.com>
2022-03-13 21:08:02 +0200

    get user appointment api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserAppointment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -10,7 +11,11 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function getUserConnections(){
-        return response()->json(array("connections" => auth()->user()->connections()));
+    // public function getUserConnections(){
+    //     return response()->json(array("connections" => auth()->user()->connections()));
+    // }
+
+    public function getUserAppointments(){
+        return response()->json(array("Appointments" => auth()->user()->clientAppointments));
     }
 }
