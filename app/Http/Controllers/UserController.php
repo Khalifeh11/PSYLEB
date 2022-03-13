d05816c3645937330dd7f51240fe2e55d5577f0b
Karim <abdelkarim.kh95@gmail.com>
2022-03-13 22:58:53 +0200

    get my logs api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,11 +14,18 @@ class UserController extends Controller
     // public function getUserConnections(){
     //     return response()->json(array("connections" => auth()->user()->connections()));
     // }
+    
+    // getting logs from client side
+    public function getMyLogs(){
+        return response()->json(array("logs" => auth()->user()->logs));
+    }
 
+    // getting appointments from client side
     public function getUserAppointments(){
         return response()->json(array("Appointments" => auth()->user()->clientAppointments));
     }
 
+    // getting appointments from provider side
     public function getProviderAppointments(){
         return response()->json(array("Appointments" => auth()->user()->providerAppointments));
     }
