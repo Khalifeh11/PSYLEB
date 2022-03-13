b94ed52c4aa9fa56611e1a2eb8a0a1753241f9b2
Karim <abdelkarim.kh95@gmail.com>
2022-03-13 23:18:57 +0200

    get client logs api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,6 +20,13 @@ class UserController extends Controller
         return response()->json(array("logs" => auth()->user()->logs));
     }
 
+    // getting client logs from provider side
+    // have to add connection condition between them
+    public function getClientLogs($id){
+        $user = User::find($id);
+        return response()->json(array("logs" => $user->logs));
+    }
+
     // getting appointments from client side
     public function getUserAppointments(){
         return response()->json(array("Appointments" => auth()->user()->clientAppointments));
