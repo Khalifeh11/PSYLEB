c922c8a3215c2be0db75d5c84505fc419095d469
Karim <abdelkarim.kh95@gmail.com>
2022-03-13 18:20:04 +0200

    request appointment API

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\UserAppointment;
+use App\Models\User;
+
+
 
 class AppointmentController extends Controller
 {
-    //
+    public function requestAppointment(Request $request){
+
+        $now = date('Y-m-d');
+        $validator = Validator::make($request->all(), [
+            'name' => 'between:0,100|string',
+            'date' =>'date_format:Y-m-d|after_or_equal:'. $now,
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+    }else{
+        $Appointment = UserAppointment::create(array_merge($validator->validated(),["client_id" => auth()->user()->id], ["provider_id"=> $request->provider_id], ["location_id"=> $request->location_id]));
+
+        return response()->json([
+            'message' => 'Appointment requested',
+            'log' => $Appointment,
+        ], 201);
+    }
+}
 }
