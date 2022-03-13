89fde9db10009d3f099fc62a57caaf113bbb7fb4
Karim <abdelkarim.kh95@gmail.com>
2022-03-13 18:45:39 +0200

    remove and approve appointment API

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -12,22 +12,59 @@ use App\Models\User;
 
 class AppointmentController extends Controller
 {
-    public function requestAppointment(Request $request){
+        public function requestAppointment(Request $request){
 
-        $now = date('Y-m-d');
-        $validator = Validator::make($request->all(), [
-            'name' => 'between:0,100|string',
-            'date' =>'date_format:Y-m-d|after_or_equal:'. $now,
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-    }else{
-        $Appointment = UserAppointment::create(array_merge($validator->validated(),["client_id" => auth()->user()->id], ["provider_id"=> $request->provider_id], ["location_id"=> $request->location_id]));
+            $now = date('Y-m-d');
+            $validator = Validator::make($request->all(), [
+                'name' => 'between:0,100|string',
+                'date' =>'date_format:Y-m-d|after_or_equal:'. $now,
+            ]);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+        }else{
+            $Appointment = UserAppointment::create(array_merge($validator->validated(),["client_id" => auth()->user()->id], ["provider_id"=> $request->provider_id], ["location_id"=> $request->location_id]));
 
-        return response()->json([
-            'message' => 'Appointment requested',
-            'log' => $Appointment,
-        ], 201);
+            return response()->json([
+                'message' => 'Appointment requested',
+                'appointment' => $Appointment,
+            ], 201);
+        }
+   
     }
-}
+
+    public function removeAppointment($id){
+
+        $appointment = UserAppointment::find($id);
+        if ($appointment->client_id == Auth::user()->id){
+            
+            $appointment->delete();
+
+            return response()->json([
+                'message'=> 'appointment succesfully deleted',
+                'appointment' => $appointment
+            ], 201);
+        }else{
+            return response()->json([
+                'message' => "This appointment doesn't belong to you"
+            ], 401);
+        }   
+    }
+
+    public function approveAppointment($id){
+
+        $appointment = UserAppointment::find($id);
+        if ($appointment->provider_id == Auth::user()->id){
+            
+            $appointment->update(['is_pending' => 0]);
+
+            return response()->json([
+                'message'=> 'appointment succesfully approved',
+                'appointment' => $appointment
+            ], 201);
+        }else{
+            return response()->json([
+                'message' => "This appointment doesn't belong to you"
+            ], 401);
+        }   
+    }
 }
