219a715e0ca583db8ecc23b1f0592d7a82ce3a52
Karim <abdelkarim.kh95@gmail.com>
2022-03-19 14:54:16 +0200

    login screen

--- psyleb-frontend/App.js
+++ psyleb-frontend/App.js
@@ -1,40 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { useState } from 'react';
-import axios from "axios";
+import Login from './screens/Login'
+
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const loginFetch = async () => {
-    const url = 'http://127.0.0.1:8000/api/auth/login';
-    const user = {
-      'email' : email,
-      'password' : password
-    }
-
-    try {
-      const response = await axios.post(url, user);
-      const data = await response.data;
-      console.log(data);
-    } catch (error) {
-      console.log("sign in error", error);
-    }
-  }
-  return (
-    <View style={styles.container}>
-      <TextInput 
-        style={styles.textInput}
-        placeholder= 'email'
-        onChangeText={(e) => setEmail(e)}
-      />
-      <TextInput 
-        style={styles.textInput}
-        placeholder= 'password'
-        onChangeText={(e) => setPassword(e)}
-      />
-      <Button title='login' onPress={loginFetch}/>
-    </View>
+  return(
+    <Login />
   );
 }
 
