6e48c1ac2d9faea2234541091b910077ec5b85e6
Karim <abdelkarim.kh95@gmail.com>
2022-03-20 17:41:12 +0200

    register screen

--- psyleb-frontend/screens/Register.js
+++ psyleb-frontend/screens/Register.js
@@ -1,26 +1,27 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import PrimaryTextInput from '../components/PrimaryTextInput'
+import { TextInput } from 'react-native-paper'
+import PrimaryButton from '../components/PrimaryButton'
 
-const Register = () => {
+const Register = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Register</Text>
+      <PrimaryTextInput label={'First Name'}/>
+      <PrimaryTextInput label={'Last Name'}/>
+      <PrimaryTextInput label={'Email'}/>
+      <PrimaryTextInput label={'Password'} isPassword={true}/>
+      <PrimaryTextInput label={'Confirm Password'} isPassword={true}/>
+      <PrimaryButton text={'register'}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      marginVertical: 50
     },
-    textInput: {
-      borderColor: 'black',
-      backgroundColor: 'green',
-      margin: 3,
-    }
   });
 
 export default Register
