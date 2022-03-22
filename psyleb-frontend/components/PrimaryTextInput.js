f7543b8c3fd6951ff76e0474352d641655df5d86
Karim <abdelkarim.kh95@gmail.com>
2022-03-23 01:53:44 +0200

    placeholder prop

--- psyleb-frontend/components/PrimaryTextInput.js
+++ psyleb-frontend/components/PrimaryTextInput.js
@@ -2,13 +2,14 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { TextInput } from "react-native-paper";
 
-const PrimaryTextInput = ({ changeText, label, isPassword, icon, value }) => {
+const PrimaryTextInput = ({ changeText, label, isPassword, icon, value, placeholder }) => {
   return (
     <View>
       <TextInput
         style={styles.textInput}
         mode="outlined"
         label={label}
+        placeholder = {placeholder}
         value={value}
         secureTextEntry={isPassword}
         activeOutlineColor="#5DB075"
