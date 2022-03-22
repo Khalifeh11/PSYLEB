a5e809bb6d749cd5893a132e141ae3f95275c065
Karim <abdelkarim.kh95@gmail.com>
2022-03-22 23:50:46 +0200

    stack switcher

--- psyleb-frontend/App.js
+++ psyleb-frontend/App.js
@@ -1,25 +1,10 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from "./screens/Login";
-import Register from "./screens/Register"
-import Navigation from "./components/Navigation";
-import Home1 from "./screens/Home1";
+import StackSwitcher from "./Routes/StackSwitcher";
 
 function App() {
-  const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Navigation" component={Navigation} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StackSwitcher />
   );
 }
-
-
 
 export default App;
