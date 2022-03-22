39cde9c6014cf78a6cde18d5e106ed1ece3c424e
Karim <abdelkarim.kh95@gmail.com>
2022-03-22 23:52:04 +0200

    mood icons component

--- psyleb-frontend/components/MoodIcons.js
+++ psyleb-frontend/components/MoodIcons.js
@@ -1,47 +1,59 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { MaterialIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const MoodIcons = () => {
   return (
     <View style={styles.container}>
-        <TouchableOpacity>
+      <TouchableOpacity>
         <MaterialIcons name="mood" size={50} color="#36D47F" />
-        <Text  style={styles.text}>Perfect</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <MaterialIcons name="sentiment-satisfied-alt" size={50} color="#75E265" 
+        <Text style={styles.text}>Perfect</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <MaterialIcons
+          name="sentiment-satisfied-alt"
+          size={50}
+          color="#75E265"
         />
         <Text style={styles.text}>Good</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
+      </TouchableOpacity>
+      <TouchableOpacity>
         <MaterialIcons name="sentiment-neutral" size={50} color="#FFD41A" />
         <Text style={styles.text}>Neutral</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <MaterialIcons name="sentiment-very-dissatisfied" size={50} color="#FF874D" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <MaterialIcons
+          name="sentiment-dissatisfied"
+          size={50}
+          color="#FE626B"
+        />
+
         <Text style={styles.text}>Bad</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <FontAwesome5 name="sad-cry" size={50} color="#FE626B" />
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <MaterialIcons
+          name="sentiment-very-dissatisfied"
+          size={50}
+          color="#FF874D"
+        />
         <Text style={styles.text}>Awful</Text>
-        </TouchableOpacity>
-
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default MoodIcons
+export default MoodIcons;
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        margin: 10
-    },
-    text: {
-        textAlign: 'center'
-    }
-})
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    margin: 10,
+  },
+  text: {
+    textAlign: "center",
+  },
+});
