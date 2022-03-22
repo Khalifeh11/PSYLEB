cd7ac9bf022cb43b38989c9db7d2e4151f161705
Karim <abdelkarim.kh95@gmail.com>
2022-03-22 23:55:36 +0200

    empty screens

---
+++ psyleb-frontend/screens/MyProviders.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MyProviders = () => {
+  return (
+    <View>
+      <Text>MyProviders</Text>
+    </View>
+  )
+}
+
+export default MyProviders
+
+const styles = StyleSheet.create({})
---
+++ psyleb-frontend/screens/Notifications.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Notifications = () => {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>Notifications</Text>
+    </View>
+    </SafeAreaView>
+
+  )
+}
+
+export default Notifications
+
+const styles = StyleSheet.create({})
