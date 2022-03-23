a5cf7fab1243309505ecbd3144f862dca8855789
Karim <abdelkarim.kh95@gmail.com>
2022-03-23 18:33:37 +0200

    removing header

--- psyleb-frontend/Routes/LoginStack.js
+++ psyleb-frontend/Routes/LoginStack.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import Login from '../screens/Login'
+import Register from '../screens/Register'
 
 const LoginStack = () => {
     const LoginNavigation = createStackNavigator();
   return (
       <LoginNavigation.Navigator>
-        <LoginNavigation.Screen name="Login" component={Login} />
+        <LoginNavigation.Screen name="Login" component={Login} options={{
+          headerShown: false
+        }}/>
         <LoginNavigation.Screen name="Register" component={Register} />
       </LoginNavigation.Navigator>
   );
