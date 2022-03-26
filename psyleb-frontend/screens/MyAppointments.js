7f20d43c1e375b442a5f7656f1c18aef415538f7
Karim <abdelkarim.kh95@gmail.com>
2022-03-27 02:57:27 +0300

    added flat list to appointments screen

--- psyleb-frontend/screens/MyAppointments.js
+++ psyleb-frontend/screens/MyAppointments.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
+import List from "../components/List";
+import { useState } from "react";
 
-const MyAppointments = ({navigation}) => {
+  
+const MyAppointments = ({ navigation }) => {
+  const [appointments,setAppointments]=useState([
+    {
+      id: 1,
+      date: '27/3/2022',
+      provider: 'Karim Khalifeh',
+      location: 'Badaro'
+    },
+    {
+      id: 2,
+      date: '27/3/2022',
+      provider: 'Charbel Daoud',
+      location: 'Badaro'
+
+    },
+    {
+      id: 3,
+      date: '27/3/2022',
+      provider: 'Joe Rizk',
+      location: 'Badaro'
+    },
+  ]);
   return (
     <View>
-      <Text>MyAppointments</Text>
+      <FlatList 
+        data={appointments}
+        key={(item) => item.id}
+        renderItem={({ item }) => (
+      // <TouchableOpacity onPress={()=>{navigation.navigate('Chat')}}>
+      <List first={item.date} second={item.provider} third={item.location} image={require("../assets/profile.jpg")} />
+      // </TouchableOpacity>
+      )} />
     </View>
-  )
-}
+  );
+};
 
-export default MyAppointments
+export default MyAppointments;
 
-const styles = StyleSheet.create({})
