b4f4906568589c909ed126e6f2128343e555441c
Karim <abdelkarim.kh95@gmail.com>
2022-03-27 02:52:02 +0300

    implemented contional stack switcher based on user context

--- psyleb-frontend/Routes/StackSwitcher.js
+++ psyleb-frontend/Routes/StackSwitcher.js
@@ -2,12 +2,19 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginStack from "./LoginStack";
 import TabNavigation from "./TabNavigation";
+import { userContext } from "../userContext";
+import { useState } from "react";
+import ProviderNavigation from './Provider/ProviderNavigation'
 
 function StackSwitcher() {
+  const [currentUser, setCurrentUser] = useState();
   return (
+    <userContext.Provider value={{ currentUser, setCurrentUser }}>
     <NavigationContainer>
-      {false ? <LoginStack /> : <TabNavigation />}
+      {!currentUser ? <LoginStack /> : currentUser.user.user_type == 1 ? <TabNavigation /> : <ProviderNavigation />}
     </NavigationContainer>
+    </userContext.Provider>
+
   );
 }
 
