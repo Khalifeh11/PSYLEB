b1c91c0b4e9ba23997fcb0803e85197dec8daefb
Karim <abdelkarim.kh95@gmail.com>
2022-03-27 03:01:45 +0300

    my providers list

--- psyleb-frontend/screens/MyProviders.js
+++ psyleb-frontend/screens/MyProviders.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const MyProviders = () => {
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import React from "react";
+import { Avatar } from "react-native-paper";
+import List from "../components/List";
+import { useState } from "react";
+  
+const MyProviders = ({}) => {
+  const [providers,setProviders]=useState([
+    {
+      id: 1,
+      name: 'Charbel Daoud',
+      job:'Clinical Psychologist',
+      City: 'Beirut'
+    },
+    {
+      id: 2,
+      name: 'Karim Khalifeh',
+      job:'Psychiatrist',
+      City: 'Saida'
+    },
+    {
+      id: 3,
+      name: 'Joe Rizk',
+      job:'Health Coach',
+      City: 'Jounieh'
+    },
+  ]);
   return (
     <View>
-      <Text>MyProviders</Text>
+      <FlatList 
+        data={providers}
+        key={(item) => item.id}
+        renderItem={({ item }) => (
+      <List first={item.name} second={item.job} third={item.City} image={require("../assets/profile.jpg")} />
+      )} />
     </View>
-  )
-}
+  );
+};
 
-export default MyProviders
+export default MyProviders;
 
-const styles = StyleSheet.create({})
