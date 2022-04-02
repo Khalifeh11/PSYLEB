99d8df707ed1058521148e0deb26b92e769d564e
Karim <abdelkarim.kh95@gmail.com>
2022-04-02 20:45:34 +0300

    my provider screen with fetch api

--- psyleb-frontend/screens/MyProviders.js
+++ psyleb-frontend/screens/MyProviders.js
@@ -1,38 +1,58 @@
-import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
 import { Avatar } from "react-native-paper";
 import List from "../components/List";
-import { useState } from "react";
-  
-const MyProviders = ({}) => {
-  const [providers,setProviders]=useState([
-    {
-      id: 1,
-      name: 'Charbel Daoud',
-      job:'Clinical Psychologist',
-      City: 'Beirut'
-    },
-    {
-      id: 2,
-      name: 'Karim Khalifeh',
-      job:'Psychiatrist',
-      City: 'Saida'
-    },
-    {
-      id: 3,
-      name: 'Joe Rizk',
-      job:'Health Coach',
-      City: 'Jounieh'
-    },
-  ]);
+import { useState, useEffect, useContext } from "react";
+import IP from "../globals/IP";
+import { userContext } from "../userContext"; 
+import axios from "axios"; 
+import { selectedProviderContext } from "../userContext";
+
+const MyProviders = ({ navigation }) => {
+  const { currentUser, setCurrentUser } = useContext(userContext);
+  const { selectedProvider, setSelectedProvider } = useContext(selectedProviderContext);
+
+  const [providers,setProviders]=useState()
+
+
+  const fetchMyProviders = async () => {
+    const url = `${IP}/api/user/myProviders`;
+    const token = currentUser.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+      const response = await axios.get(url, config);
+      const dataFetched = response.data;
+      setProviders(dataFetched);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+useEffect(fetchMyProviders, []);
+// console.warn(providers && providers.MyProviders)
+
   return (
     <View>
+      
       <FlatList 
-        data={providers}
+        data={providers && providers.MyProviders}
         key={(item) => item.id}
         renderItem={({ item }) => (
-      <List first={item.name} second={item.job} third={item.City} image={require("../assets/profile.jpg")} />
+      <TouchableOpacity onPress={
+        ()=>{
+          setSelectedProvider(item)
+          navigation.navigate('MyProvidersProfile')
+        }
+        }>
+      <List first={item.first_name + ' ' + item.last_name} second={item.occupation} third={item.City} image={require("../assets/profile.jpg")} />
+      </TouchableOpacity>
       )} />
+
+
     </View>
   );
 };
