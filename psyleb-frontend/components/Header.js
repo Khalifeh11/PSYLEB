f40542d3c63cf48f9f1f299ca1a99fad19e2404f
Karim <abdelkarim.kh95@gmail.com>
2022-04-02 20:27:36 +0300

    settings icon prop instead of job

--- psyleb-frontend/components/Header.js
+++ psyleb-frontend/components/Header.js
@@ -1,28 +1,24 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { Avatar } from "react-native-paper";
-import { Ionicons } from '@expo/vector-icons';  
+import { Ionicons } from "@expo/vector-icons";
 
-
-const Header = ({ job }) => {
+const Header = ({ settings }) => {
   return (
     <View>
       <View style={styles.header}>
-          <View style={styles.avatarContainer}>
-            <Avatar.Image
-              size={130}
-              source={require("../assets/profile.jpg")}
-              style={styles.avatar}
-            />
-          </View>
-
-          <View style={styles.logoutContainer}>
-            {/* <Text style={styles.logout}>Logout</Text> */}
-            <TouchableOpacity onPress={job}>
-            <Ionicons name="settings-outline" size={30} color="white" />
-            </TouchableOpacity>
-          </View>
+        <View style={styles.avatarContainer}>
+          <Avatar.Image
+            size={130}
+            source={require("../assets/profile.jpg")}
+            style={styles.avatar}
+          />
         </View>
+
+        <View>
+          {settings}
+        </View>
+      </View>
     </View>
   );
 };
@@ -30,22 +26,22 @@ const Header = ({ job }) => {
 export default Header;
 
 const styles = StyleSheet.create({
-    header: {
-        backgroundColor: "rgba(91, 176, 117, 1)",
-        height: 150,
-      },
-      avatarContainer: {
-        alignSelf: "center",
-        marginTop: 60,
-      },
-      logoutContainer: {
-        alignSelf: "flex-end",
-        marginTop: -170,
-        marginRight: 20,
-      },
-      logout: {
-        fontWeight: "bold",
-        fontSize: 17,
-        color: "white",
-      },
+  header: {
+    backgroundColor: "rgba(91, 176, 117, 1)",
+    height: 150,
+  },
+  avatarContainer: {
+    alignSelf: "center",
+    marginTop: 60,
+  },
+  logoutContainer: {
+    alignSelf: "flex-end",
+    marginTop: -170,
+    marginRight: 20,
+  },
+  logout: {
+    fontWeight: "bold",
+    fontSize: 17,
+    color: "white",
+  },
 });
