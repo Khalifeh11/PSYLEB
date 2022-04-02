6b8b8bd631129ce6e82e0908135c2e6441ebb2cf
Karim <abdelkarim.kh95@gmail.com>
2022-04-02 20:38:43 +0300

    added selected provider context to stack switcher

--- psyleb-frontend/Routes/StackSwitcher.js
+++ psyleb-frontend/Routes/StackSwitcher.js
@@ -2,17 +2,20 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginStack from "./LoginStack";
 import TabNavigation from "./TabNavigation";
-import { userContext } from "../userContext";
+import { userContext, selectedProviderContext } from "../userContext";
 import { useState } from "react";
 import ProviderNavigation from './Provider/ProviderNavigation'
 
 function StackSwitcher() {
   const [currentUser, setCurrentUser] = useState();
+  const [selectedProvider, setSelectedProvider] = useState();
   return (
     <userContext.Provider value={{ currentUser, setCurrentUser }}>
+    <selectedProviderContext.Provider value={{ selectedProvider, setSelectedProvider }}>
     <NavigationContainer>
       {!currentUser ? <LoginStack /> : currentUser.user.user_type == 1 ? <TabNavigation /> : <ProviderNavigation />}
     </NavigationContainer>
+    </selectedProviderContext.Provider>
     </userContext.Provider>
 
   );
