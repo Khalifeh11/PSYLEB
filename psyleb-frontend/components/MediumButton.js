00b8957db0d997c10785d8f26d94e9cc6c8c790c
Karim <abdelkarim.kh95@gmail.com>
2022-04-02 20:30:12 +0300

    medium button component

---
+++ psyleb-frontend/components/MediumButton.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { Button } from 'react-native-paper'
+
+const MediumButton = ({ job, text }) => {
+  return (
+    <View style={styles.button}>
+        <Button mode='contained' color='#5DB075' onPress={job}>
+            <Text style={styles.buttonText}>{text}</Text>
+        </Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        margin: 10,
+        width: 250,
+    },
+    buttonText: {
+      color: 'white'
+    }
+})
+
+export default MediumButton
