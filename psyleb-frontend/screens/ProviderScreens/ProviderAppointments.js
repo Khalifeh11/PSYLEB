9ba46845b0df1e024d388a981e2d60518435e0cd
Karim <abdelkarim.kh95@gmail.com>
2022-04-02 20:51:33 +0300

    provider appointments as a provider screen with fetch api

--- psyleb-frontend/screens/ProviderScreens/ProviderAppointments.js
+++ psyleb-frontend/screens/ProviderScreens/ProviderAppointments.js
@@ -1,14 +1,126 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import List from "../../components/List";
+import AppointmentList from "../../components/AppointmentList";
+import { useState } from "react";
+import IP from "../../globals/IP";
+import { userContext } from "../../userContext";
+import { useContext } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import format from "date-fns/format";
+import { IconButton } from "react-native-paper";
 
-const ProviderAppointments = () => {
+const ProviderAppointments = ({ navigation }) => {
+  const { currentUser, setCurrentUser } = useContext(userContext);
+  // console.warn(currentUser);
+  const [appointments, setAppointments] = useState();
+
+  const fetchProviderAppointments = async () => {
+    const url = `${IP}/api/user/provider-appointments`;
+    const token = currentUser.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+      const response = await axios.get(url, config);
+      const dataFetched = response.data;
+      setAppointments(dataFetched);
+      // console.warn(appointments.appointments);
+      // console.warn(dataFetched);
+    } catch (error) {
+      console.warn(error, "cant get appointments");
+    }
+  };
+
+  // useEffect(() => {
+  //   fetchAppointments();
+  // }, []);
+
+  useEffect(fetchProviderAppointments, []);
+
+  const approveAppointment = async (id) => {
+    const url = `${IP}/api/appointment/approve/${id}`;
+    const token = currentUser.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+      const response = await axios.get(url, config);
+      const dataFetched = response.data;
+      fetchProviderAppointments();
+    } catch (error) {
+      console.warn(error, "cant approve appointment");
+    }
+  };
+
+  const declineAppointment = async (id) => {
+    const url = `${IP}/api/appointment/decline/${id}`;
+    const token = currentUser.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+      const response = await axios.delete(url, config);
+      const dataFetched = response.data;
+      fetchProviderAppointments();
+    } catch (error) {
+      console.warn(error, "cant approve appointment");
+    }
+  };
+
+  const todayDate = new Date();
   return (
     <View>
-      <Text>ProviderAppointments</Text>
+      <FlatList
+        data={appointments && appointments.appointments}
+        key={(item) => item.id}
+        renderItem={({ item }) => (
+          <AppointmentList
+            first={item.datetime}
+            second={
+              item.is_pending === 1
+                ? item.first_name + " " + item.last_name + " (pending)"
+                : item.first_name + " " + item.last_name + " (approved)"
+            }
+            third={item.city + " Clinic"}
+            image={require("../../assets/profile.jpg")}
+            icon1={
+              item.is_pending === 1 ? (
+                <IconButton
+                  color={"#5DB075"}
+                  icon="checkbox-marked"
+                  onPress={() => approveAppointment(item.id)}
+                />
+              ) : (
+                null
+              )
+            }
+            icon2={
+              item.is_pending === 1 ? 
+              (<IconButton
+                color={"red"}
+                icon="delete-outline"
+                onPress={() => declineAppointment(item.id)}
+              />) : (null)
+            }
+          />
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default ProviderAppointments
-
-const styles = StyleSheet.create({})
+export default ProviderAppointments;
