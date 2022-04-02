28e2dabe2ef3a606bafac7cf27bf10466e34f340
Karim <abdelkarim.kh95@gmail.com>
2022-04-02 20:31:14 +0300

    color prop to primary button component

--- psyleb-frontend/components/PrimaryButton.js
+++ psyleb-frontend/components/PrimaryButton.js
@@ -2,10 +2,10 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native-paper'
 
-const PrimaryButton = ({ job, text }) => {
+const PrimaryButton = ({ job, text, color }) => {
   return (
     <View style={styles.button}>
-        <Button mode='contained' color='#5DB075' onPress={job}>
+        <Button mode='contained' color={color} onPress={job}>
             <Text style={styles.buttonText}>{text}</Text>
         </Button>
     </View>
