8bcdd5a6ef310e7e710c33d2383f0f00f7b555ca
Karim <abdelkarim.kh95@gmail.com>
2022-04-05 18:33:57 +0300

    added add profile pic feature and error handling for text inputs

--- psyleb-frontend/screens/Settings.js
+++ psyleb-frontend/screens/Settings.js
@@ -1,26 +1,88 @@
 import React from "react";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import IP from "../globals/IP";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Image,
+  TouchableOpacity,
+  // Button
+} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import axios from "axios";
 import { userContext } from "../userContext";
 import PrimaryTextInput from "../components/PrimaryTextInput";
 import { TextInput } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Button } from "react-native-paper";
+
 const Settings = () => {
   const { currentUser, setCurrentUser } = useContext(userContext);
+
+  // handling image upload
+
+  const [image, setImage] = useState();
+  const [base64Img, setbase64Img] = useState();
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      setbase64Img(result.base64);
+    }
+  };
+
+  const updateImg = async () => {
+    const url = "http://192.168.0.109:8000/api/user/addPicture";
+    const token = currentUser.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const imagetoSend = {
+      image: base64Img,
+    };
+
+    try {
+      const response = await axios.post(url, imagetoSend, config);
+      const responseData = response.data;
+      setCurrentUser({
+        ...currentUser,
+        user: {
+          ...currentUser.user,
+          profile_pic: responseData.profile_pic,
+        },
+      });
+
+      Alert.alert("Image Uploaded");
+    } catch (er) {
+      console.warn(er);
+    }
+  };
+
   const token = currentUser.access_token;
+
   const updateProfileAPI = `${IP}/api/user/edit-profile`;
 
+  // profile data update
   const [profiledata, setProfileData] = useState({
     first_name: "",
     last_name: "",
     email: "",
-    password: "",
-    password_confirmation: "",
     bio: "",
+    phone_number: "",
   });
 
+  // handling first name
   const handleFirstName = (value) => {
     setProfileData({
       ...profiledata,
@@ -28,13 +90,15 @@ const Settings = () => {
     });
   };
 
+  // handling last name
   const handleLastName = (value) => {
-    setProfileData({  
+    setProfileData({
       ...profiledata,
       last_name: value,
     });
   };
 
+  // handling email
   const handleEmail = (value) => {
     setProfileData({
       ...profiledata,
@@ -42,20 +106,7 @@ const Settings = () => {
     });
   };
 
-  const handlePassword = (value) => {
-    setProfileData({
-      ...profiledata,
-      password: value,
-    });
-  };
-
-  const handlePasswordConfirmation = (value) => {
-    setProfileData({
-      ...profiledata,
-      password_confirmation: value,
-    });
-  };
-
+  // handling bio
   const handleBio = (value) => {
     setProfileData({
       ...profiledata,
@@ -63,44 +114,128 @@ const Settings = () => {
     });
   };
 
+  // handling phone number
+  const handlePhone = (value) => {
+    setProfileData({
+      ...profiledata,
+      phone_number: value,
+    });
+  };
+
+  // async function to update profile
+
   const updateProfile = async () => {
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
-    }
-  };
+      },
+    };
     // first_name= currentUser.user.first_name,
-    const user = {
+    const userData = {
       first_name: profiledata.first_name,
       last_name: profiledata.last_name,
       email: profiledata.email,
-      password: profiledata.password,
-      password_confirmation: profiledata.password_confirmation,
       bio: profiledata.bio,
+      phone_number: profiledata.phone_number,
     };
-    try {
-      const response = await axios.post(updateProfileAPI, user, config);
-      const dataFetched = response.data;
-      setCurrentUser(dataFetched);
-      // console.warn(currentUser)
-//     for (const key in user) {
-//       if(user[key] === ''){
-//         key=== 'first_name' ? handleFirstName(currentUser.user.first_name) :
-//         key=== 'last_name' ? handleLastName(currentUser.user.last_name) :
-//         key=== 'email' ? handleEmail(currentUser.user.email) :
-//         key=== 'bio' ? handleBio(currentUser.user.bio) :
-//         console.log(key)
-//   }
-// }
 
+    userData.first_name == ""
+      ? (userData.first_name = currentUser.user.first_name)
+      : (userData.first_name = profiledata.first_name);
+
+    userData.last_name == ""
+      ? (userData.last_name = currentUser.user.last_name)
+      : (userData.last_name = profiledata.last_name);
+
+    userData.email == ""
+      ? (userData.email = currentUser.user.email)
+      : (userData.email = profiledata.email);
+
+      userData.bio == ""
+      ? (userData.bio = currentUser.user.bio)
+      : (userData.bio = profiledata.bio);
+
+
+    userData.phone_number == ""
+      ? (userData.phone_number = currentUser.user.phone_number)
+      : (userData.phone_number = profiledata.phone_number);
+
+    try {
+      if ( currentUser.user.bio == null && userData.bio ==  null) {
+        userData.bio = "";
+      }
+      if (
+        (currentUser.user.phone_number == null &&
+          userData.phone_number == null) 
+      ) 
+      
+      {
+        Alert.alert("Make sure you have phone number");
+      
+      
+      }else {
+        const response = await axios.post(updateProfileAPI, userData, config);
+        const dataFetched = response.data;
+        setCurrentUser({
+          ...currentUser,
+          user: {
+            ...currentUser.user,
+            first_name: dataFetched.user.first_name,
+            last_name: dataFetched.user.last_name,
+            email: dataFetched.user.email,
+            bio: dataFetched.user.bio,
+            phone_number: dataFetched.user.phone_number,
+          },
+        });
+        Alert.alert("Profile Updated");
+      }
     } catch (error) {
-      console.warn(error);
+      console.warn(userData);
     }
   };
-
   const logout = () => setCurrentUser(null);
+
+  // console.warn(`${IP}/public${currentUser.user.profile_pic}`)
+
   return (
     <View style={styles.container}>
+      {/* image section  */}
+      <View style={styles.imgContainer}>
+        {currentUser.user.profile_pic == null ? (
+          <Image
+            source={{
+              uri: "https://ca.slack-edge.com/T0NC4C7NK-U039444J2UR-g1e75ab176a1-512",
+            }} //http://192.168.1.95:8000
+            style={styles.profileImage}
+          />
+        ) : (
+          <Image
+            source={{
+              uri: `${IP}${currentUser.user.profile_pic}`,
+            }}
+            style={styles.profileImage}
+          />
+        )}
+
+        <TouchableOpacity onPress={pickImage} style={{ bottom: 30, left: 30 }}>
+          <Ionicons name="md-create" size={30} color="black" />
+        </TouchableOpacity>
+
+        <View style={styles.imgBtnContainer}>
+          <Button
+            mode="text"
+            onPress={updateImg}
+            style={styles.imgBtn}
+            color="#008080"
+          >
+            Save
+          </Button>
+        </View>
+        {/* <Button title="confirm upload" onPress={updateImg}/> */}
+      </View>
+
+      {/* first name section */}
+
       <View>
         <TextInput
           label={"First Name"}
@@ -110,8 +245,12 @@ const Settings = () => {
           onChangeText={handleFirstName}
           defaultValue={currentUser.user.first_name}
           value={profiledata.first_name}
+          left={<TextInput.Icon name="account-circle" />}
         />
       </View>
+
+      {/* last name section */}
+
       <View>
         <TextInput
           label={"Last Name"}
@@ -121,8 +260,12 @@ const Settings = () => {
           onChangeText={handleLastName}
           defaultValue={currentUser.user.last_name}
           value={profiledata.last_name}
+          left={<TextInput.Icon name="account-circle" />}
         />
       </View>
+
+      {/* email section */}
+
       <View>
         <TextInput
           label={"Email"}
@@ -132,30 +275,27 @@ const Settings = () => {
           onChangeText={handleEmail}
           defaultValue={currentUser.user.email}
           value={profiledata.email}
+          left={<TextInput.Icon name="email" />}
         />
       </View>
+
+      {/* bio section */}
+
       <View>
         <TextInput
-          label={"Password"}
+          label={"Phone number"}
           selectionColor={"green"}
           underlineColor={"black"}
           activeUnderlineColor={"green"}
-          onChangeText={handlePassword}
-          // value={currentUser.user.password}
-          // defaultValue={currentUser.user.password}
+          onChangeText={handlePhone}
+          defaultValue={currentUser.user.phone_number}
+          value={profiledata.phone_number}
+          left={<TextInput.Icon name="phone" />}
         />
       </View>
-      <View>
-        <TextInput
-          label={"Confirm Password"}
-          selectionColor={"green"}
-          underlineColor={"black"}
-          activeUnderlineColor={"green"}
-          onChangeText={handlePasswordConfirmation}
-          // value={currentUser.user.password_confirmation}
-          // defaultValue={currentUser.user.password_confirmation}
-        />
-      </View>
+
+      {/* bio section */}
+
       <View>
         <TextInput
           label={"Bio"}
@@ -166,14 +306,18 @@ const Settings = () => {
           onChangeText={handleBio}
           defaultValue={currentUser.user.bio}
           value={profiledata.bio}
+          left={<TextInput.Icon name="text" />}
         />
       </View>
-      <View style={styles.logoutButton}>
-        <PrimaryButton text={"Save"} job={updateProfile} color={"#5DB075"}/>
+
+      {/*Buttons */}
+
+      <View style={styles.saveBtn}>
+        <PrimaryButton text={"Save"} job={updateProfile} color={"#042a2b"} />
       </View>
 
-      <View style={styles.logoutButton}>
-        <PrimaryButton text={"logout"} job={logout} color={'#5DB075'}/>
+      <View style={styles.logoutBtn}>
+        <PrimaryButton text={"logout"} job={logout} color={"#d84727"} />
       </View>
     </View>
   );
@@ -182,8 +326,41 @@ const Settings = () => {
 export default Settings;
 
 const styles = StyleSheet.create({
-  logoutButton: {
+  saveBtn: {
     marginTop: 20,
     alignSelf: "center",
   },
+
+  logoutBtn: {
+    alignSelf: "center",
+  },
+
+  container: {
+    marginHorizontal: 20,
+    marginTop: 30,
+  },
+
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    borderWidth: 3,
+    borderColor: "#5DB075",
+    marginTop: 20,
+  },
+
+  imgContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+    marginTop: -20,
+    // backgroundColor: 'red',
+    alignItems: "center",
+  },
+
+  imgBtnContainer: {
+    alignItems: "center",
+    marginTop: -30,
+  },
+
+  imgBtn: {},
 });
