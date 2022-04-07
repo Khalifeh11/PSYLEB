996b43dd3c17dd6cbf0041333d42196ad8fccc34
Karim <abdelkarim.kh95@gmail.com>
2022-04-07 20:31:35 +0300

    add phone number to user model

--- psyleb-backend/app/Models/User.php
+++ psyleb-backend/app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements JWTSubject
         'bio',
         'password',
         'dob',
+        'phone_number',
         'occupation',
         'education',
         'gender',
