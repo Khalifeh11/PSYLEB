ccafddd69df68bfd22c7916d19e06ea86c3c953f
Karim <abdelkarim.kh95@gmail.com>
2022-04-08 19:52:08 +0300

    added aws IP

--- psyleb-frontend/globals/IP.js
+++ psyleb-frontend/globals/IP.js
@@ -1,4 +1,23 @@
-const IP = "http://192.168.1.95:8000";
+// bdd net
+// const IP = "http://192.168.1.95:8000"; 
+
+// const IP = 'http://192.168.1.17:8000';
+
+// home wifi
+// const IP = "http://192.168.0.109:8000"; 
+
+// amazon ip
+
+const IP = 'http://ec2-3-129-61-232.us-east-2.compute.amazonaws.com:3000';
+
+
+// 3g network
+// const IP = 'http://192.168.53.213:8000';
+// http://192.168.0.213:8000
+
+
+
+
 
 export default IP;
 
