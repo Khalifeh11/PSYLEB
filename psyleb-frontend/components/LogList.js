6dd31f7412e4c1a0c94737b910b885f7d1ee44eb
Karim <abdelkarim.kh95@gmail.com>
2022-04-08 19:04:59 +0300

    log list component fixes

--- psyleb-frontend/components/LogList.js
+++ psyleb-frontend/components/LogList.js
@@ -1,21 +1,24 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
-import { Avatar } from "react-native-paper";
 
-const LogList = ({ first, second, third, icon}) => {
+
+const LogList = ({ first, second, third, icon, deleteIcon }) => {
   return (
     <View>
-        <View style={styles.providerContainer}>
-          <View style={styles.avatarContainer}>
-              {icon}
+      <View style={styles.providerContainer}>
+        <View style={styles.avatarContainer}>{icon}</View>
+        <View style={styles.providerInfoContainer}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.firstText}>{first}</Text>
+            <TouchableOpacity style={styles.deleteContainer}>
+              {deleteIcon}
+            </TouchableOpacity>  
           </View>
-          <View style={styles.providerInfoContainer}>
-            <Text style={styles.first}>{first}</Text>
-            <Text style={styles.second}>{second}</Text>
-            <Text style={styles.third}>{third}</Text>
-            <View style={styles.hr}></View>
-          </View>
+          <Text style={styles.second}>{second}</Text>
+          <Text style={styles.third}>{third}</Text>
+          <View style={styles.hr}></View>
         </View>
+      </View>
     </View>
   );
 };
@@ -32,18 +35,30 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
 
-  first: {
+  headerContainer: {
+    flexDirection: "row",
+    // backgroundColor: 'green',
+   },
+
+  firstText: {
     fontWeight: "bold",
     fontSize: 17,
+    flex: 0.57,
   },
+
+  // deleteContainer: {
+  //   justifyContent: 'flex-end',
+  //   alignContent: 'flex-end',
+  // },
+
   second: {
-    fontStyle: 'italic',
-    color: '#7a7a7a',
+    fontStyle: "italic",
+    color: "#7a7a7a",
     maxWidth: 250,
   },
   third: {
     maxWidth: 250,
-    marginTop: 10
+    marginTop: 10,
   },
   hr: {
     borderBottomColor: "rgba(0,0,0,0.2)",
