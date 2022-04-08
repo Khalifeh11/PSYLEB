adce1ab38077c54ac46ac6f731823894c54f7c89
Karim <abdelkarim.kh95@gmail.com>
2022-04-08 19:06:01 +0300

    medium button component fixes

--- psyleb-frontend/components/MediumButton.js
+++ psyleb-frontend/components/MediumButton.js
@@ -2,10 +2,10 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native-paper'
 
-const MediumButton = ({ job, text }) => {
+const MediumButton = ({ job, text, disabled }) => {
   return (
     <View style={styles.button}>
-        <Button mode='contained' color='#5DB075' onPress={job}>
+        <Button mode='contained' color='#5DB075' onPress={job} disabled={disabled}>
             <Text style={styles.buttonText}>{text}</Text>
         </Button>
     </View>
